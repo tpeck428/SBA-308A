867677b8b7d933b0bef38916dc0f9beb5f5599e6
tpeck428 <toripeck73@gmail.com>
2024-03-24 23:44:40 -0400

    working async/await function

--- index.js
+++ index.js
@@ -2,11 +2,27 @@
 // const API_url = 'http://www.omdbapi.com/?i=tt3896198&apikey=a0d03957';
 // const apikey = 'a0d03957'
 
-//imports
-import { fetchPoster } from "./modules/mod1";
+// //imports
+// import { fetchPoster } from "./modules/mod1";
+// console.log(fetchPoster);
 
 
 const randomMovie = document.getElementById('random-movie');
-const saveMovie = document.getElementById('save');
+const saveMovie = document.getElementById('save')
+
+async function fetchMovie() {
+    try {
+        const response = await fetch('http://www.omdbapi.com/?i=tt3896198&apikey=a0d03957'); // Replace with your API endpoint
+        if (!response.ok) {
+            throw new Error(`Error fetching data: ${response.status}`);
+        }
+        const data = await response.json();
+        console.log('Fetched data:', data);
+        // Process the data as needed (e.g., update UI, display results)
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+}
 
 
+document.getElementById('random-movie').addEventListener('click', fetchMovie);
--- modules/mod1.js
+++ modules/mod1.js
@@ -3,19 +3,20 @@
 // const posterUrl = "http://img.omdbapi.com/?apikey=a0d03957&"
 // const apikey = 'a0d03957'
 
-export async function fetchPoster() {
-    try {
-        const response = await fetch('http://img.omdbapi.com/?apikey=a0d03957&'); // Replace with your API endpoint
-        if (!response.ok) {
-            throw new Error(`Error fetching data: ${response.status}`);
-        }
-        const data = await response.json();
-        console.log('Fetched data:', data);
-        // Process the data as needed (e.g., update UI, display results)
-    } catch (error) {
-        console.error('Error fetching data:', error);
-    }
-}
+// export async function fetchMovie() {
+//     try {
+//         const response = await fetch('http://www.omdbapi.com/?i=tt3896198&apikey=a0d03957'); 
+//         if (!response.ok) {
+//             throw new Error(`Error fetching data: ${response.status}`);
+//         }
+//         const data = await response.json();
+//         console.log('Fetched data:', data);
+//         // Process the data as needed (e.g., update UI, display results)
+//     } catch (error) {
+//         console.error('Error fetching data:', error);
+//     }
+// }
 
 
-document.getElementById('save').addEventListener('click', fetchDataFromAPI);
+// document.getElementById('random-movie').addEventListener('click', fetchMovie);
+
