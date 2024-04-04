2f405bf9853bf21877e5b12ad51cf28ae519084d
tpeck428 <toripeck73@gmail.com>
2024-04-04 00:51:37 -0400

    working event listener

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 const url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+
+const result = document.getElementById("result");
+const sound = document.getElementById("sound");
+
+const btn = document.getElementById("search-btn");
+
+btn.addEventListener('click', () => {
+    let input = document.getElementById('input').value;
+    // console.log(input);
+})
